2f94ff7fa6becde2f6213107dc5c5e999db16624
Ross <rosso5792@gmail.com>
2019-12-08 22:05:59 +0000

    added more customers and films

--- console.rb
+++ console.rb
@@ -11,22 +11,40 @@ customer1 = Customer.new({
   "name" => "Billy",
   "funds" => 100
   })
-customer1.save()
 
+customer2 = Customer.new({
+  "name" => "Jimmy",
+  "funds" => 50
+  })
+
+customer1.save()
+customer2.save()
 
 film1 = Film.new({
   "title" => "Home Alone",
   "price" => 5
   })
-film1.save()
 
+film2 = Film.new({
+  "title" => "Dumb and Dumber",
+  "price" => 10
+  })
+
+  film1.save()
+  film2.save()
 
 ticket1 = Ticket.new({
   "customer_id" => customer1.id,
   "film_id" => film1.id
   })
-ticket1.save()
 
+ticket2 = Ticket.new({
+  "customer_id" => customer2.id,
+  "film_id" => film2.id
+  })
+
+ticket1.save()
+ticket2.save()
 
 binding.pry
 nil
